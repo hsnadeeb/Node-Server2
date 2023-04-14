114a8fcd0a75c432732f766467a97c74b4eb52ee
Hasan Adeeb <hsnadeeb007@gmail.com>
2023-04-14 14:25:04 +0530

    displayed the message in the html by reading it from file.

--- app.js
+++ app.js
@@ -1,30 +1,50 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    res.setHeader('Content-Type', 'text/html');
-    if (req.url === '/home') {
+//     console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    const method = req.method;
+    if (req.url === '/') {
         res.write('<html>');
-        res.write('<head><title>My first Page</title><head>');
-        res.write('<body><h1>Welcome home</h1></body>');
-        res.write('</html>');
-    } else if (req.url === '/about') {
-        res.write('<html>');
-        res.write('<head><title>My first Page</title><head>');
-        res.write('<body><h1>Welcome to About Us page</h1></body>');
-        res.write('</html>');
-    } else if (req.url === '/node') {
-        res.write('<html>');
-        res.write('<head><title>My first Page</title><head>');
-        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
-        res.write('</html>');
-    } else {
-        res.write('<html>');
-        res.write('<head><title>My first Page</title><head>');
-        res.write('<body><h1>Page not found</h1></body>');
-        res.write('</html>');
+        res.write('<head><title>Submit form</title><head>');
+        try {
+            const message = fs.readFileSync('message.txt', 'utf8');
+            res.write(`<p>Previous message: ${message}</p>`);
+        } catch (err) {
+            console.log(err);
+        }
+        res.write('<body><form action="/message" method="POST" ><input type="text" name="message"><button type="submit">Send<button></form>');
+
+        res.write('</body></html>');
+        return res.end();
     }
-    res.end();
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            console.log(message);
+            fs.writeFileSync('message.txt', message);
+
+          });
+
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
+    }
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Hello</title><head>');
+        res.write('<body><h1>Hello</h1></body>');
+        res.write('</html>');
+        res.end();
+
 });
 
-server.listen(5500);
+server.listen(3000);
