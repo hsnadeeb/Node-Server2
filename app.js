8d05d4265b8fe5a77e03f934f16ff2d8056c9264
Hasan Adeeb <hsnadeeb007@gmail.com>
2023-04-17 16:34:16 +0530

    worked with app.use, app.post and other functions

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 // const http = require('http');
 // const routes= require('./routes')
 const express = require('express');
-
+const bodyParser=require('body-parser');
 const app=express();
 
-app.use((req,res,next)=>{
+app.use(bodyParser.urlencoded({extended:false}));
 
-    console.log("in the middelware");
-    next();
+// app.use('/',(req,res,next)=>{
+
+//     console.log("This always runs");
+//     next();
+// });
+
+app.use('/add-product',(req,res,next)=>{
+
+    // console.log("in the next middelware");
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>')
 });
-app.use((req,res,next)=>{
+ app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
 
-    console.log("in the next middelware");
-    res.send('<h1>Hello from second ! middleware</h1>')
+ })
+
+app.use('/',(req,res,next)=>{
+
+    // console.log("in the next middelware");
+    res.send('<h1>Middleware Page</h1>')
 });
 
 // const server = http.createServer(app);
